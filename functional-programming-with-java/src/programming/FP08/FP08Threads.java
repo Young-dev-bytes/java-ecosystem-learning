f17e2e447096653ae33d35d299268ad0e49a4acd
chenqiuyang <1182492999@qq.com>
2023-12-23 18:18:06 +0800

    1223

--- functional-programming-with-java/src/programming/FP08/FP08Threads.java
+++ functional-programming-with-java/src/programming/FP08/FP08Threads.java
@@ -1,5 +1,8 @@
 package programming.FP08;
 
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
 public class FP08Threads {
 
     public static void main(String[] args) {
@@ -16,11 +19,16 @@ public class FP08Threads {
 
         };
 
-        Runnable runnable2 = () -> {
+        /*Runnable runnable2 = () -> {
             for (int i = 0; i < 10000; i++) {
                 System.out.println(Thread.currentThread().getId() + ":" + i);
 
             }
+        };*/
+
+        Runnable runnable2 = () -> {
+            // IntStream.range(0,10000).forEach(System.out::println);
+            IntStream.range(0,10000).forEach(FP08Threads::getIntConsumer);
         };
 
         Thread thread = new Thread(runnable2);
@@ -31,5 +39,9 @@ public class FP08Threads {
 
         Thread thread2 = new Thread(runnable2);
         thread2.start();
+    }
+
+    private static void getIntConsumer(int i) {
+        System.out.println(Thread.currentThread().getId() + ":" + i);
     }
 }
