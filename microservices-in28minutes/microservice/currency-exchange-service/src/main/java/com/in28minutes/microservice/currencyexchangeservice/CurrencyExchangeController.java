3cdef27921d754b1dc4f6824fb82f099e3d60e31
chenqiuyang <1182492999@qq.com>
2023-12-25 17:30:05 +0800

    1225

---
+++ microservices-in28minutes/microservice/currency-exchange-service/src/main/java/com/in28minutes/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,33 @@
+package com.in28minutes.microservice.currencyexchangeservice;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+public class CurrencyExchangeController {
+
+
+    private final Environment environment;
+
+    private final ExchangeValueRepository exchangeValueRepository;
+
+    public CurrencyExchangeController(Environment environment, ExchangeValueRepository exchangeValueRepository) {
+        this.environment = environment;
+        this.exchangeValueRepository = exchangeValueRepository;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(
+                                                @PathVariable String from,
+                                                @PathVariable String to) {
+
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from,to);
+        exchangeValue.setPort(
+                Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        return exchangeValue;
+    }
+}
