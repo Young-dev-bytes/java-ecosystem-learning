24a35bfac09979ea7cbdba9f017b6a56620ec189
chenqiuyang <1182492999@qq.com>
2023-12-29 15:01:36 +0800

    1229

--- microservices-in28minutes/microservice/currency-exchange-service/src/main/java/com/in28minutes/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ microservices-in28minutes/microservice/currency-exchange-service/src/main/java/com/in28minutes/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservice.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +17,10 @@ public class CircuitBreakerController {
             LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    // @Retry(name = "default")
-    @Retry(name = "sample-api",fallbackMethod = "hardcodedResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    //@RateLimiter(name="default")
+    @Bulkhead(name="sample-api")
     public String sampleApi() {
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
