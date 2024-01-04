14e5ae6404d40f2854c564521180c6d166a779db
chenqiuyang <1182492999@qq.com>
2024-01-04 10:30:06 +0800

    0104

--- microservices-in28minutes/microservice-v3/currency-exchange-service/src/main/java/com/in28minutes/microservice/currencyexchangeservice/ExchangeValue.java
+++ microservices-in28minutes/microservice-v3/currency-exchange-service/src/main/java/com/in28minutes/microservice/currencyexchangeservice/ExchangeValue.java
@@ -2,9 +2,10 @@ package com.in28minutes.microservice.currencyexchangeservice;
 
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
 @Entity
