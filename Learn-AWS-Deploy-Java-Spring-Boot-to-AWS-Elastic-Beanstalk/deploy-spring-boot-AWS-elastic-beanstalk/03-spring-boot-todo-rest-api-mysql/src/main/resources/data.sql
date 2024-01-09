0104447e4f01901034218b576af7cceb84c7d0d4
chenqiuyang <1182492999@qq.com>
2024-01-09 17:15:10 +0800

    0109

---
+++ Learn-AWS-Deploy-Java-Spring-Boot-to-AWS-Elastic-Beanstalk/deploy-spring-boot-AWS-elastic-beanstalk/03-spring-boot-todo-rest-api-mysql/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'in28minutes', 'Learn JPA', current_date(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'in28minutes', 'Learn Data JPA', current_date(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'in28minutes', 'Learn Microservices', current_date(), false);
