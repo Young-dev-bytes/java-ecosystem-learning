2747ad11cabf839492713bc22bef9edd9c094fcd
Young <63534114+Young-dev-bytes@users.noreply.github.com>
2024-08-07 00:35:58 +0800

    Delete 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp/dockerfile directory

--- 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp/dockerfile/code-server-4.89.0-linux-amd64/lib/vscode/bin/remote-cli/code.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set ROOT_DIR=%~dp0..\..\..\..
-set VSROOT_DIR=%~dp0..\..
-call "%ROOT_DIR%\node.exe" "%VSROOT_DIR%\out\server-cli.js" "code-server" "1.89.0" "0f4791b816c184b612df3bdcfc4e1f9f1a13daca" "code-server.cmd" %*
-endlocal
